07acdc0d22d860e23bdff50ae6166d7f2a1ba566
LukaPrepadnik <luka.prepadnik@student.um.si>
2024-04-24 14:48:12 +0200

    priprava frontenda

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,44 @@
-export default function Home() {
+import AcmeLogo from './ui/acme-logo';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Page() {
   return (
-    <p>Hello world!</p>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-3 py-10 md:w-2/5 md:px-20">
+          <p className="text-xl text-gray-800 md:text-3xl md:leading-normal"
+            style={{ fontFamily: "'Lusitana', serif" }}>
+            <strong>Welcome to Acme!</strong>
+            <br />
+            Explore the future of secure and efficient data sharing with our advanced data management platform.
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span>
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* <Image
+            className="hidden md:block mr-20"
+            src="/123.png"
+            width={200}
+            height={200}
+            alt="Acme"
+          />
+          <Image
+            src="/456.png"
+            width={200}
+            height={200}
+            alt="Acme"
+          /> */}
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ frontend/app/ui/acme-logo.tsx
@@ -0,0 +1,7 @@
+export default function AcmeLogo() {
+  return (
+    <div className="flex flex-row items-center leading-none text-white">
+      <p style={{ fontFamily: "'Lusitana', serif", fontSize: '44px' }}>Acme</p>
+    </div>
+  );
+}
