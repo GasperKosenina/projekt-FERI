6c94f0f3e08b978ce142142ccd2a221ccb8303ac
JanVolovsek <jan.volovsek@student.um.si>
2024-05-03 19:39:45 +0200

    Naredil izgled prve strani

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,51 +1,57 @@
 import AcmeLogo from "./ui/acme-logo";
 import Link from "next/link";
 import Image from "next/image";
+import "../app/styles/animations.css";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-3 py-10 md:w-2/5 md:px-20">
+      <ul className="background">
+        {Array.from({ length: 18 }).map((_, idx) => (
+          <li key={idx}></li>
+        ))}
+      </ul>
+      {/* <div className="mt-4 flex grow flex-col gap-4 md:flex-row"> */}
+      <div className="flex items-center justify-center min-h-screen">
+        <div
+          className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-3 py-10 md:w-2/5 md:px-20"
+          style={{ backgroundColor: "#ffffff" }}
+        >
           <p
             className="text-xl text-gray-800 md:text-3xl md:leading-normal"
-            style={{ fontFamily: "'Lusitana', serif" }}
+            style={{ fontFamily: "'Lusitana', serif", textAlign: "center" }}
           >
-            <strong>Welcome to Acme!</strong>
+            <Image
+              src="/DS_logo2.png"
+              alt="Data Chain Logo"
+              width={200}
+              height={100}
+              priority
+              style={{ margin: "0 auto" }}
+            />
+
             <br />
-            Explore the future of secure and efficient data sharing with our
-            advanced data management platform.
+            <span style={{ fontSize: "24px", fontFamily: "Helvetica" }}>
+              Welcome to DataChain, your starting point for easy access to data.
+            </span>
           </p>
-          <Link
-            href="/sign-up"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Sign Up</span>
-          </Link>
-          <Link
-            href="/sign-in"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Sign In</span>
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* <Image
-            className="hidden md:block mr-20"
-            src="/123.png"
-            width={200}
-            height={200}
-            alt="Acme"
-          />
-          <Image
-            src="/456.png"
-            width={200}
-            height={200}
-            alt="Acme"
-          /> */}
+          <SignedIn>
+            <Link
+              href="/dashboard"
+              className="flex items-center justify-center gap-5 rounded-lg bg-blue-500 px-4 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Continue</span>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <Link
+              href="/sign-in"
+              className="flex items-center justify-center gap-5 rounded-lg bg-blue-500 px-4 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Sign In</span>
+            </Link>
+          </SignedOut>
         </div>
       </div>
     </main>
