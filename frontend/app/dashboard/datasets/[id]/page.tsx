9b971231d82a7f84eab451b49cb7b5c7c4c4f15a
LukaPrepadnik <luka.prepadnik@student.um.si>
2024-05-13 20:20:23 +0200

    nove podrobnosti

--- frontend/app/dashboard/datasets/[id]/page.tsx
+++ frontend/app/dashboard/datasets/[id]/page.tsx
@@ -28,6 +28,9 @@ export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
 
   const dataset: Dataset = await findById(id);
+  //console.log(dataset)
+
+
 
   const access_token = await generate(dataset.accessToken);
 
@@ -49,38 +52,48 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <div className="max-w mx-auto bg-gray-100 p-8 rounded-md">
-        <h2 className="text-xl font-semibold mb-4">{dataset.name}</h2>
-        <div className="space-y-4">
-          <div>
-            <p className="text-gray-500">URL:</p>
-            <p>{dataset.url}</p>
-          </div>
-          <div>
-            <p className="text-gray-500">Price:</p>
-            <p>{dataset.price}€</p>
-          </div>
-          <div>
-            <p className="text-gray-500">Category:</p>
-            <p>{dataset.category}</p>
-          </div>
-          <div>
-            <p className="text-gray-500">Data Provider:</p>
-            <p><span>{(await getDataProvider(dataset.userID))} </span></p>
-          </div>
-          <div>
-            <p className="text-gray-500">Duration:</p>
-            <p>{dataset.duration} seconds</p>
-          </div>
-          <div>
-            <p className="text-gray-500">Date:</p>
-            <p>{formatDate(dataset.createdAt)}</p>
-          </div>
+
+
+      <div>
+        <div className="mt-20 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{dataset.name}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">URL</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{dataset.url}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Data Provider</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"><span>{(await getDataProvider(dataset.userID))} </span></dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Category</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{dataset.category}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Price</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{dataset.price}€</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Duration</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{dataset.duration}€</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Date</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {formatDate(dataset.createdAt)}
+              </dd>
+            </div>
+
+          </dl>
         </div>
       </div>
-      
 
-      <p className="mt-20">Your access token:</p>
+
+      <p className="mt-52">Your access token:</p>
       <strong>{access_token.access_token}</strong>
     </main>
   );
