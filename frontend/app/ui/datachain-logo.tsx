c909be2c41a3a9d7be190ad6453a1721f1f0da7d
JanVolovsek <jan.volovsek@student.um.si>
2024-05-15 15:33:48 +0200

    spremembe v navbaru

--- frontend/app/ui/datachain-logo.tsx
+++ frontend/app/ui/datachain-logo.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+
 export default function DataChainLogo() {
   return (
-    <div className="flex flex-row items-center leading-none text-white">
-      <p style={{ fontFamily: "'Lusitana', serif", fontSize: "38px" }}>
-        DataChain
-      </p>
+    <div className="flex flex-row items-center leading-none text-white p-4 rounded">
+      <Image
+        src="/DS_logo2.png" // Uporabite pot do slike, ki ste jo naložili
+        alt="Data Chain Logo"
+        width={120} // Spremenjena širina slike
+        height={130} // Spremenjena višina slike
+        priority
+        className="rounded-lg" // Dodan Tailwind CSS razred za zaobljene robove
+        style={{ marginBottom: "-13px", marginLeft: "-11px" }} // Prilagojene margine
+      />
     </div>
   );
 }
