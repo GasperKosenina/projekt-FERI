3617d9e629e5b8a437e17954bc97a41072854082
JanVolovsek <jan.volovsek@student.um.si>
2024-05-21 17:02:05 +0200

    posodobitev payment succesfull strani

--- frontend/app/dashboard/paypal/success/page.tsx
+++ frontend/app/dashboard/paypal/success/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({
 }) {
   const datasetId = searchParams?.datasetId;
   const payment_id = searchParams?.payment_id;
-  console.log(payment_id)
+  console.log(payment_id);
 
   if (datasetId == null) {
     return <p>Dataset not found</p>;
@@ -19,24 +19,22 @@ export default async function Page({
   }
 
   const dataset: Dataset = await findById(datasetId);
-  const payment = await getPaymentById(payment_id)
+  const payment = await getPaymentById(payment_id);
 
-  await updateStatus(payment_id)
+  await updateStatus(payment_id);
 
   let access_token;
 
-
   if (!payment.accessToken) {
-    access_token = await generate(dataset.accessToken);
-    await updateToken(payment_id);
-  }
-
+  access_token = await generate(dataset.accessToken);
+  await updateToken(payment_id);
+   }
 
   if (access_token == null) {
-    return <p>You have already receive your access token!</p>;
+    return <p>You have already received your access token!</p>;
   }
 
   return (
-    <PaymentSuccess access_token={access_token.access_token} />
+    <PaymentSuccess access_token={access_token.access_token} dataset={dataset} />
   );
 }
