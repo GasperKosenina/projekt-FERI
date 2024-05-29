3c8416b4ce71eb0bb618bc074a8f77b7581688cc
JanVolovsek <jan.volovsek@student.um.si>
2024-05-29 12:21:37 +0200

    you don't have access to this page - stran

--- frontend/app/dashboard/datasets/published/[id]/page.tsx
+++ frontend/app/dashboard/datasets/published/[id]/page.tsx
@@ -2,16 +2,16 @@ import { findById, getAllPaymentsByDataset } from "@/lib/data";
 import { Dataset, Payment } from "@/lib/definitions";
 import DatasetPage from "@/app/ui/published-dataset";
 import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const dataset: Dataset = await findById(id);
     const payments: Payment[] = await getAllPaymentsByDataset(id);
 
-
     const durationInMilliseconds = dataset.duration * 60 * 60 * 1000;
 
-    const validCunsomers = payments.filter(payment => {
+    const validCustomers = payments.filter(payment => {
         if (!payment.tokenCreatedAt) {
             return false;
         }
@@ -20,7 +20,7 @@ export default async function Page({ params }: { params: { id: string } }) {
         return (currentTime - tokenCreatedAt) < durationInMilliseconds;
     });
 
-    const validCunsomersLength = validCunsomers.length;
+    const validCustomersLength = validCustomers.length;
 
     const { userId } = auth();
     if (!userId) {
@@ -29,9 +29,31 @@ export default async function Page({ params }: { params: { id: string } }) {
     }
 
     if (dataset.userID !== userId) {
-        return <p>You do not have access to this page!</p>;
+        return (
+            <div>
+                <h1
+                    className="text-left font-normal text-gray-500 mb-4"
+                    style={{ fontSize: "1.375rem" }}
+                >
+                    Access Denied
+                </h1>
+                <div className="rounded-lg p-6 bg-[#f9fafb]">
+                    <div className="mt-6 animate-bounce">
+                        <p className="text-center mt-10 text-xl text-grey-500">
+                        Uh-oh! ☹ You do not have access to this page. Please try again later.
+                        </p>
+                    </div>
+                    <div className="flex justify-center mt-10 mb-8">
+                        <Link href={`/dashboard/datasets`}>
+                            <div className="inline-block px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-[#60a5fa]">
+                                Go to Dashboard
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
     }
 
-
-    return <DatasetPage dataset={dataset} validCunsomers={validCunsomersLength} />;
+    return <DatasetPage dataset={dataset} validCunsomers={validCustomersLength} />;
 }
